77aac2e43519c9be83b543880c37e158f9a3af0f
chudroy <nijotigajo@gmail.com>
2022-09-03 18:32:39 +0200

    add animation and scroll to function

--- workoholics-test/src/app/contact-flag/contact-flag.component.ts
+++ workoholics-test/src/app/contact-flag/contact-flag.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact-flag',
   templateUrl: './contact-flag.component.html',
-  styleUrls: ['./contact-flag.component.scss']
+  styleUrls: ['./contact-flag.component.scss'],
 })
 export class ContactFlagComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  scroll(el: string) {
+    const scrollTo = document.getElementsByTagName(el)[0];
+    console.log(scrollTo);
+
+    scrollTo?.scrollIntoView({ behavior: 'smooth' });
   }
-
 }
